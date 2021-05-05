a8fc8c846ae5fbd7225f128a9427fd13847b848b
Bryce Egley <begley@factoryexpohomes.com>
2021-05-05 16:06:17 -0700

    Added global mode variable

--- aquarium-controller.ino
+++ aquarium-controller.ino
@@ -3,11 +3,22 @@
 #include "TemperatureProbe.h"
 #include "LightController.h"
 
+// globals
+/**
+ * 
+ * Modes:
+ * 0 = automatic
+ * 1 = manual
+ * 2 = evening display
+ * 3 = thunderstorm
+ * 
+ **/
+
+byte mode = 0;
+
 void setup() {
     Serial.begin(115200);
-    Serial.println("************************************");
-    Serial.println("Starting Bryce's Aquarium Controller");
-    Serial.println("************************************");
+    Serial.println("Starting controller...");
 
     setupNetwork();
 
